e9844970b6a487bafd6515cbb7a9c4cc25befdac
Ismail Jalloh <ijalloh21@gmail.com>
2018-12-29 13:37:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  vitamins = { "Salmon" => "Vitamin B12, Fatty-3 Acid" }
   
 end
 
